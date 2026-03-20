928f3dac780be81fbb04a93d13864c5010da4f8a
hatem-m <116768361+hatem-m@users.noreply.github.com>
2026-03-20 06:47:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-// server.js
-const jsonServer = require("json-server");
-const path = require("path");
+const jsonServer = require('json-server');
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults({ noCors: false });
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
 
-// استخدم middlewares مثل CORS
 server.use(middlewares);
-
-// عند فتح Home "/" نعرض كل محتويات db.json
-server.get("/", (req, res) => {
-  res.json(router.db.getState());
-});
-
-// استخدم جميع المسارات الافتراضية
 server.use(router);
 
 const PORT = process.env.PORT || 8080;
